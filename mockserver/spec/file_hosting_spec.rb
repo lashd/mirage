4f6bd4de672d49f6c702837d1b60570c61cce129
Leon Davis <leon_a_d@yahoo.com>
2011-02-18 05:17:10 +0000

    tidy up

--- mockserver/spec/file_hosting_spec.rb
+++ mockserver/spec/file_hosting_spec.rb
@@ -10,21 +10,19 @@ describe 'hosting files on the mockserver' do
     @spec_dir = File.dirname(__FILE__)
     @download_path = "#{@spec_dir}/download.zip"
     FileUtils.rm_f(@download_path)
+    @test_file = File.new("#{@spec_dir}/test.zip")
 
     @browser = Mechanize.new
     get("/mockserver/clear")
   end
 
   it 'should host a file' do
-    test_file_path = "#{@spec_dir}/test.zip"
-    test_file = File.new(test_file_path)
-
-    post("/mockserver/set/file_response", :file=>test_file)
+    post("/mockserver/set/file_response", :file=>@test_file)
 
     download = get("/mockserver/get/file_response")
-    download.filename.should == File.basename(test_file.path)
     download.save_as(@download_path)
 
-    FileUtils.cmp(test_file_path, @download_path).should == true
+    download.filename.should == File.basename(@test_file.path)
+    FileUtils.cmp(@test_file.path, @download_path).should == true
   end
 end
