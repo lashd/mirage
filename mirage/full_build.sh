79b5bb890c4cb86b1e04ce542e5f6e34ede2d081
Leon Davis <leon_a_d@yahoo.com>
2011-03-06 20:25:23 +0000

    added help for using script

--- mirage/full_build.sh
+++ mirage/full_build.sh
@@ -1,45 +1,63 @@
 #!/bin/bash
-
-
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-
-  source "$HOME/.rvm/scripts/rvm"
-
-elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-
-  \# Then try to load from a root install
-  source "/usr/local/rvm/scripts/rvm"
-
-else
-    echo -e "RVM must be installed to run this script. \n
-             Until it is installed simply run the default rake target to test Mirage against your active version of Ruby and installed gems"
-fi
-
-
-
-source "$HOME/.rvm/scripts/rvm"
-
 blue='\033[34m'
 red='\033[31m'
 green='\033[32m'
 yellow='\033[33m'
 white='\033[37m'
+bold='\033[1m'
+reset='\033[0m'
+
+print(){
+    echo -e "$1${reset}"
+}
+
+usage(){
+    print "${bold}Usage:\n"
+    print "./full_build.sh [ruby_version]\n"
+    print "When running with out a ruby version, the full build is run for ruby versions:"
+    print "1.8.6\n1.8.7\n1.9.1\n1.9.2\njruby\n"
+    print "Else specify a particular ruby version to run the build against\n"
+}
+
+
+if [ -f "$HOME/.rvm/scripts/rvm" ]
+then
+  source "$HOME/.rvm/scripts/rvm"
+elif [ -f "/usr/local/rvm/scripts/rvm" ]
+then
+  source "/usr/local/rvm/scripts/rvm"
+else
+    print "${bold}RVM Not found"
+    print "I looked in $HOME/.rvm/scripts/rvm and /usr/local/rvm/scripts/rvm"
+    print "RVM must be installed to run this script. It's great! find out more: here ${bold}http://rvm.beginrescueend.com/"
+    print "Until it is installed simply run the default rake target to test Mirage against your active version of Ruby and installed gems"
+    exit 1
+fi
+
+while getopts ":h" opt; do
+  case $opt in
+    h)
+      usage
+      exit 0
+      ;;
+    \?)
+      print "Invalid option: -$OPTARG"
+      usage
+      exit 1
+      ;;
+  esac
+done
 
 message=""
 result=true
 
-reset_comandline_colours(){
-    tput sgr0
-}
-
 run_build_for_ruby( ){
-    echo -e "${green}Running build for: $1"
-    reset_comandline_colours
+    print "${green}Running build for: $1"
 
     ruby_list=`rvm list`
     if [[ ${ruby_list} == *$1* ]]
     then
-        rvm $1
+        rvm --create $1@mirage
         rvm --force gemset empty
         [ -f Gemfile.lock ] && rm Gemfile.lock
         gem install bundler
@@ -63,16 +81,14 @@ if [ $1 ]
 then
     run_build_for_ruby $1
 else
-    run_build_for_ruby 'ruby-1.8.2'
-    run_build_for_ruby 'ruby-1.8.1'
+    run_build_for_ruby 'ruby-1.8.6'
+    run_build_for_ruby 'ruby-1.8.7'
+    run_build_for_ruby 'ruby-1.9.1'
     run_build_for_ruby 'ruby-1.9.2'
-    run_build_for_ruby 'ruby-1.9.4'
-    run_build_for_ruby 'jruby-1.5.4'
+    run_build_for_ruby 'jruby'
 fi
 
-echo -e "\n\n${message}"
-echo -ne "${white}Result: "
-[ ${result} == true ] && echo -e "${green}Pass\n" || echo -e "${red}Fail\n"
-reset_comandline_colours
-echo ""
+print "\n\n${message}"
+print "${white}Result: "
+[ ${result} == true ] && print "${green}Pass\n" || print "${red}Fail\n"
 
