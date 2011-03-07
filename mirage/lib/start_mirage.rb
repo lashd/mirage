ba3bfe8efe5c507bcd80997925a730095cca39fc
Leon Davis <leon_a_d@yahoo.com>
2011-03-07 05:03:06 +0000

    simplified starting of mirage

--- mirage/lib/start_mirage.rb
+++ mirage/lib/start_mirage.rb
@@ -7,7 +7,7 @@ include Mirage::Util
 
 options = parse_options(ARGV)
 
-DEFAULT_RESPONSES_DIR = File.directory?(options[:defaults_directory]) ? "#{options[:defaults_directory]}" : "#{options[:root_directory]}/#{options[:defaults_directory]}"
+DEFAULT_RESPONSES_DIR = "#{options[:defaults_directory]}"
 Mirage.client = Mirage::Client.new(options)
 Ramaze.start :port => options[:port]
 
