3b10ec74c818856ea03ef703c2c06d5cd9d4ad4a
Leon Davis <leon_a_d@yahoo.com>
2011-08-14 17:26:44 +0100

    tidy up

--- lib/start_mirage.rb
+++ lib/start_mirage.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 $0='Mirage Server'
 ROOT_DIR = File.dirname(__FILE__)
 $LOAD_PATH.unshift(ROOT_DIR)
- 
+
 require 'sinatra'
 require 'mirage/server'
 require 'mirage/client'
@@ -11,23 +11,25 @@ include Mirage::Util
 
 module Mirage
   class Server < Sinatra::Base
-    configure do 
+    
+    configure do
       options = parse_options(ARGV)
       set :defaults_directory, options[:defaults_directory]
       set :port, options[:port]
-      Mirage.client = Mirage::Client.new "http://localhost:#{options[:port]}/mirage"
-      
+      set :show_exceptions, false
+      set :logging, true
+      set :server, 'webrick'
+      set :views, "#{ROOT_DIR}/views"
+
       log_file = File.open('mirage.log', 'a')
       log_file.sync=true
       use Rack::CommonLogger, log_file
-      
       enable :logging
-      set :views, "#{ROOT_DIR}/views"
-      set :show_exception, false
+      Mirage.client = Mirage::Client.new "http://localhost:#{options[:port]}/mirage"
     end
   end
 end
 
-Mirage::Server.run! :show_exceptions => false, :logging => true, :server => 'webrick'
+Mirage::Server.run! 
 
 
