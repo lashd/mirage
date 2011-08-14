49a9448acb61eb46d8df5dfe69646407ac76cbeb
Leon Davis <leon_a_d@yahoo.com>
2011-08-14 18:21:58 +0100

    removing needless files

--- lib/mirage/server.rb
+++ lib/mirage/server.rb
@@ -1,14 +1,35 @@
-$LOAD_PATH.unshift "#{File.dirname(__FILE__)}"
+require 'rubygems'
+$0='Mirage Server'
+ROOT_DIR = File.dirname(__FILE__)
+$LOAD_PATH.unshift(ROOT_DIR)
+
 require 'client'
 require 'sinatra/base'
 require 'object'
 require 'mock_response'
 require 'mock_responses_collection'
 
+include Mirage::Util
+
 
 module Mirage
 
   class Server < Sinatra::Base
+    
+    configure do
+      options = parse_options(ARGV)
+      set :defaults_directory, options[:defaults_directory]
+      set :port, options[:port]
+      set :show_exceptions, false
+      set :logging, true
+      set :server, 'webrick'
+      set :views, "#{ROOT_DIR}/../../views"
+
+      log_file = File.open('mirage.log', 'a')
+      log_file.sync=true
+      use Rack::CommonLogger, log_file
+      enable :logging
+    end
 
     REQUESTS= {}
 
@@ -126,3 +147,6 @@ module Mirage
     end
   end
 end
+
+
+Mirage::Server.run!
