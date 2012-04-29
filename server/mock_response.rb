667b4961583506c8928798ab81beee10cbcd0021
lashd <team@ubuntu.(none)>
2012-04-29 22:33:57 +0100

    default status code instead of a custom getter

--- server/mock_response.rb
+++ server/mock_response.rb
@@ -106,7 +106,7 @@ module Mirage
 
     def initialize name, value,options
       @name, @value = name, value
-      @options = {:pattern => :basic, :http_method => 'GET', :delay => 0.0}.merge(options){|key, old_value, new_value| new_value || old_value}
+      @options = {:pattern => :basic, :http_method => 'GET', :delay => 0.0, :status_code => 200}.merge(options){|key, old_value, new_value| new_value || old_value}
       @options[:http_method].upcase!
       MockResponse.add self
     end
@@ -139,10 +139,6 @@ module Mirage
 
       end
       value
-    end
-
-    def status_code
-      @options[:status_code] || 200
     end
 
     private
