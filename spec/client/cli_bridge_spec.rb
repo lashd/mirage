4468822150ac6dcaf83e9b57586484df07a0bdc3
lashd <leon_a_d@yahoo.com>
2013-04-21 22:43:01 +0100

    clean up test

--- spec/client/cli_bridge_spec.rb
+++ spec/client/cli_bridge_spec.rb
@@ -1,68 +1,63 @@
 require 'spec_helper'
 require 'mirage/client'
 
-describe Mirage::CLIBridge do
-
-
-  before :each do
-    @bridge = Object.new
-    @bridge.extend(Mirage::CLIBridge)
+describe CLIBridge do
+  def mapping port
+    {port.to_s => port_pid_mappings[port.to_s]}
   end
 
-  describe 'Windows' do
+  let(:port_pid_mappings) do
+    {"7001" => "18903", "7002" => "18904", "7003" => "18905"}
+  end
 
-    include_context :windows
+  let(:operating_system) do
+    Hashie::Mash.new({
+                         :windows => {
+                             :kill_string => "taskkill /F /T /PID %d",
+                             :set_ps_cmd_expectation => proc{bridge.should_receive(:`).with(/tasklist.*/).any_number_of_times.and_return(tasklist_output)}
+                         },
+                         :linux => {
+                             :kill_string => "kill -9 %d",
+                             :set_ps_cmd_expectation => proc{IO.should_receive(:popen).with(/ps aux.*/).any_number_of_times.and_return(tasklist_output)}
+                         }
+                     })
+  end
 
-    it 'should find the pids of mirage instances for given ports' do
+  [:linux,:windows].each do |os_name|
 
-      tasklist_output = "#{process_string_for_mirage(7001, 18903)}
-      #{process_string_for_mirage(7002, 18904)}
-      #{process_string_for_mirage(7003, 18905)}"
+    describe os_name do
+      let(:os){operating_system[os_name]}
+      let!(:bridge) do
+        bridge = Object.new
+        bridge.extend(CLIBridge)
+      end
 
-      @bridge.should_receive(:`).with(/tasklist.*/).any_number_of_times.and_return(tasklist_output)
-      @bridge.mirage_process_ids([7001, 7002]).should == {"7001" => "18903", "7002" => "18904"}
-    end
+      include_context os_name do
 
-    it 'should find the pids of mirage instances for all ports' do
-      tasklist_output = "#{process_string_for_mirage(7001, 18903)}
-      #{process_string_for_mirage(7002, 18904)}
-      #{process_string_for_mirage(7003, 18905)}"
+        let(:tasklist_output) do
+          output = []
+          port_pid_mappings.each do |port, pid|
+            output << process_string_for_mirage(port, pid)
+          end
+          output.join("\n")
+        end
 
-      @bridge.should_receive(:`).with(/tasklist.*/).any_number_of_times.and_return(tasklist_output)
-      @bridge.mirage_process_ids([:all]).should == {"7001" => "18903", "7002" => "18904", "7003" => "18905"}
-    end
+        it 'should find the pids of mirage instances for given ports' do
+          os.set_ps_cmd_expectation.call
+          bridge.mirage_process_ids([7001, 7002]).should == mapping(7001).merge(mapping(7002))
+        end
 
-    it 'should kill the given process id' do
-      @bridge.should_receive(:`).with(/taskkill \/F \/T \/PID 18903/)
-      @bridge.kill(18903)
+        it 'should find the pids of mirage instances for all ports' do
+          os.set_ps_cmd_expectation.call
+          bridge.mirage_process_ids([:all]).should == port_pid_mappings
+        end
+
+        it 'should kill the given process id' do
+          bridge.should_receive(:`).with(os.kill_string % 18903)
+          bridge.kill(18903)
+        end
+      end
     end
   end
 
-  describe 'Linux/MacOSX' do
-
-    include_context :linux
-
-    it 'should find the pids of mirage instances for given ports' do
-      ps_aux_output = "#{process_string_for_mirage(7001, 18903)}
-      #{process_string_for_mirage(7002, 18904)}
-      #{process_string_for_mirage(7003, 18905)}"
-
-      IO.should_receive(:popen).with(/ps aux.*/).any_number_of_times.and_return(ps_aux_output)
-      @bridge.mirage_process_ids([7001, 7002]).should == {"7001" => "18903", "7002" => "18904"}
-    end
-
-    it 'should find the pids of mirage instances for all ports' do
-      ps_aux_output = "#{process_string_for_mirage(7001, 18903)}
-      #{process_string_for_mirage(7002, 18904)}
-      #{process_string_for_mirage(7003, 18905)}"
-
-      IO.should_receive(:popen).with(/ps aux.*/).any_number_of_times.and_return(ps_aux_output)
-      @bridge.mirage_process_ids([:all]).should == {"7001" => "18903", "7002" => "18904", "7003" => "18905"}
-    end
-
-    it 'should kill the given process id' do
-      @bridge.should_receive(:`).with(/kill -9 18903/)
-      @bridge.kill(18903)
-    end
-  end
 end
