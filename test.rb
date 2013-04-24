5bbf0872121f740ebaebeb1f215d00ddb6859ef9
lashd <leon_a_d@yahoo.com>
2013-04-25 00:35:52 +0100

    example

--- test.rb
+++ test.rb
@@ -15,3 +15,4 @@ end
 Mirage.stop
 mirage = Mirage.start
 mirage.put ServiceNowResponse.new.this('foo').that('bar')
+mirage.put ServiceNowResponse.new.this('foo').that('bar').required_body_content(%w(hello world))
