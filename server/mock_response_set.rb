345c9512b7bcc233120aacdad1447edfad5ff2bc
Leon Davis <leon_a_d@yahoo.com>
2018-01-16 19:30:06 +0530

    Update mock_response_set.rb

--- server/mock_response_set.rb
+++ server/mock_response_set.rb
@@ -1,10 +1,14 @@
 module Mirage
   class MockResponseSet < Hash
-    def fuzzy_find desired_key
+    def fuzzy_find desired_key, http_method
       result = self[desired_key]
       return result unless result.nil?
-      key, value = find{|key, value| key.is_a?(Regexp) && desired_key.is_a?(String) && key.match(desired_key) }
-      value
+      result = find_all do |key, value|
+        key.is_a?(Regexp) && desired_key.is_a?(String) && key.match(desired_key) && value[http_method]
+      end.sort do |a,b|
+        b.first.source.size <=> a.first.source.size
+      end
+      result.first && result.first[1]
     end
   end
 end
